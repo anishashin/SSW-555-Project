2bfd2c9e4f3823e2d56d67b6a00b0fa9198f4d4f
anishashin <60356251+anishashin@users.noreply.github.com>
2020-06-18 22:35:26 -0400

    Add files via upload

---
+++ Project 04 Test.py
@@ -0,0 +1,12 @@
+import unittest
+from datetime import date
+import Project 04
+
+class Test(unittest.TestCase):
+
+    def test01(self):
+        birthday = date(8, 2, 2021)
+        #self.assertEqual()
+
+if __name__ == "__main__":
+    unittest.main()
