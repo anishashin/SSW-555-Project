6929423a0bc095708d9c5c70b5d94dabf50ef661
anishashin <60356251+anishashin@users.noreply.github.com>
2020-06-30 10:08:26 -0400

    Add files via upload

---
+++ Project06_Test.py
@@ -0,0 +1,43 @@
+import unittest
+import Project06
+from datetime import date
+from Project06 import us01DatesBeforeCurrentDate
+from Project06 import us02BirthBeforeMarriage
+from Project06 import us03BirthBeforeDeath
+from Project06 import us04MarriageBeforeDivorce
+from Project06 import us07LessThan150YearsOld
+from Project06 import us08BirthBeforeMarriageOfParents
+
+class Test(unittest.TestCase):
+
+    def test01(self):
+        self.assertEqual(us01DatesBeforeCurrentDate('2 AUG 2021', 'US01_I01', 'INDIVIDUAL', 'Birthday'), 'ERROR: ' + 'INDIVIDUAL' + ': US01: ' + 'US01_I01' + ': ' + 'Birthday' + ' ' + '2021-08-02' + ' occurs in the future')
+        self.assertEqual(us01DatesBeforeCurrentDate('22 AUG 2020', 'US01_I08', 'INDIVIDUAL', 'Death'), 'ERROR: ' + 'INDIVIDUAL' + ': US01: ' + 'US01_I08' + ': ' + 'Death' + ' ' + '2020-08-22' + ' occurs in the future')
+        self.assertEqual(us01DatesBeforeCurrentDate('27 AUG 2020', 'US01US02_F01', 'FAMILY', 'Divorce date'), 'ERROR: ' + 'FAMILY' + ': US01: ' + 'US01US02_F01' + ': ' + 'Divorce date' + ' ' + '2020-08-27' + ' occurs in the future')
+        self.assertEqual(us01DatesBeforeCurrentDate('13 SEP 2022', 'US01US02_F03', 'FAMILY', 'Marriage date'), 'ERROR: ' + 'FAMILY' + ': US01: ' + 'US01US02_F03' + ': ' + 'Marriage date' + ' ' + '2022-09-13' + ' occurs in the future')
+
+    def test02(self):
+        self.assertEqual(us02BirthBeforeMarriage('23 MAY 2020', 'US01US02_F01'), ['ERROR: FAMILY: US02: ' + 'US01US02_F01' + ": Husband's birth date " + '2094-03-01' + ' after marriage date ' + '2020-05-23', 'ERROR: FAMILY: US02: ' + 'US01US02_F01' + ": Wife's birth date " + '2021-08-02' + ' after marriage date ' + '2020-05-23'])
+
+    def test03(self):
+        self.assertEqual(us03BirthBeforeDeath('US01US03_I11'), 'ERROR: INDIVIDUAL: US03: ' + 'US01US03_I11' + ': Died ' + '1995-04-26' + ' before born ' + '2035-01-01')
+
+    def test04(self):
+        self.assertEqual(us04MarriageBeforeDivorce('US04US08_F02'), 'ERROR: FAMILY: US04: ' + 'US04US08_F02' + ': Divorced ' + '1995-11-29' + ' before married ' + '1997-11-29')
+
+    def test07(self):
+        self.assertEqual(us07LessThan150YearsOld('US07_I05'), ['ERROR: INDIVIDUAL: US07: US07_I05: More than 150 years old - Birth date 1867-09-23'])
+        self.assertEqual(us07LessThan150YearsOld('US07_I10'), ['ERROR: INDIVIDUAL: US07: US07_I10: More than 150 years old - Birth date 1843-01-15', 'ERROR: INDIVIDUAL: US07: US07_I10: More than 150 years old at death - Birth 1843-01-15: Death 2020-06-30'])
+
+    def test08(self):
+        self.assertEqual(us08BirthBeforeMarriageOfParents('US01US02US08_F03'), ['ANOMALY: FAMILY: US08: US01US02US08_F03: Child I09 born 2018-09-14 before marriage on 2022-09-13'])
+
+    def test09(self):
+        pass
+
+    def test10(self):
+        pass
+
+if __name__ == "__main__":
+    Project06.main()
+    unittest.main()
